8f67e899a38e82b9c1d4526e8921d041b94fe361
Youssef11khaled99 <uossefkhaled99@gmail.com>
2019-12-21 13:03:30 +0200

    Adding Register

--- back-end/Controllers/register.js
+++ back-end/Controllers/register.js
@@ -1,33 +1,61 @@
 const handleRegister = (req, res, db, bcrypt) => {
-    const { name, email, password } = req.body;
-    if (!email || !name || !password) {
+    const {
+        C_ID,
+        USERNAME,
+        EMAIL,
+        PASSWORD,
+        TELEPHONE
+    } = req.body;
+    if (!C_ID ||
+        !USERNAME ||
+        !EMAIL ||
+        !PASSWORD ||
+        !TELEPHONE) {
         return res.status(400).json('incorrect form submission');
     }
     const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    name: name,
-                    email: loginEmail[0],
-                    joined: new Date()
-            })
-            .then(user => res.json(user[0]))
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    
-    .catch(err => res.status(400).json('unable to register!!'))
-}
+    db('CANDIDATE').insert({
+        C_ID: C_ID,
+        USERNAME: USERNAME,
+        EMAIL: EMAIL,
+        PASSWORD: hash,
+        TELEPHONE: TELEPHONE
 
+    }).then(function (result) {
+        res.json({
+            success: true,
+            message: 'ok'
+        }); // respond back to request
+    }).catch(err => res.status(400).json('unable to register!!'))
+
+}
 module.exports = {
     handleRegister: handleRegister
 }
+
+/**
+ *
+ */
+/* 
+db.transaction(trx => {
+    trx.insert({
+            PASSWORD: hash,
+            EMAIL: email
+        })
+        .into('login')
+        .returning('EMAIL')
+        .then(loginEmail => {
+            return trx('CANDIDATE')
+                .returning('*')
+                .insert({
+                    USERNAME: name,
+                    EMAIL: loginEmail[0],
+                    TELEPHONE: TELEPHONE
+                })
+                .then(user => res.json(user[0]))
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+})
+
+*/
--- back-end/server.js
+++ back-end/server.js
@@ -4,21 +4,31 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 
-const register = require("./controllers/register");
-const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
+const register = require("./Controllers/register");
+const signin = require("./Controllers/signin");
+const profile = require("./Controllers/profile");
+const exam = require("./Controllers/exam");
 
 const db = knex({
     client: "mysql",
+    // connection: {
+    //     host: "sql7.freemysqlhosting.net",
+    //     user: "sql7316459",
+    //     password: "CXnZqH3VKJ",
+    //     database: "sql7316459",
+    //     port: "3306"
+    // }
+
+    // Local
     connection: {
-        host: "sql7.freemysqlhosting.net",
-        user: "sql7316459",
-        password: "CXnZqH3VKJ",
-        database: "sql7316459",
+        host: "http://127.0.0.1/",
+        user: "root",
+        password: "1234",
+        database: "online_recruiter",
         port: "3306"
     }
 
-    // We will change to this after deploying on Heroku
+    // Heroku
     // connection: {
     //     host: process.env.CLEARDB_DATABASE_URL,
     //     user: "b2035d16df9aff",
@@ -37,9 +47,15 @@ app.get("/", (req, res) => {
     res.send("It's working");
 });
 
-app.get("/signin", (req, res) => {
+app.post("/signin", (req, res) => {
     res.send("sigin Working");
     signin.handleSignin(req, res);
+});
+
+
+app.get("/exam", (req, res) => {
+    console.log("u")
+    res.send("exam Working");
 });
 
 app.post("/register", (req, res) => {
@@ -49,6 +65,6 @@ app.post("/register", (req, res) => {
 app.get("/profile/:userId", (req, res) => {
     profile.handleProfileGet(req, res, db);
 });
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running in port ${process.env.PORT}`); // The same as db url we put them in the host
+app.listen(process.env.PORT || 5500, () => {
+    console.log(`app is running in port 5500`); // The same as db url we put them in the host
 });
