7c07e9e14e9a0fca269917887c80844af0ff6c60
Youssef11khaled99 <uossefkhaled99@gmail.com>
2019-12-22 07:18:53 +0200

    Removing the bootstrap.js

--- back-end/src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-/* bootstrap.js */
-route = require("./Routes");
-//Default function that will bootstrap the routes and link it with controllers
-module.exports = (app, router) => {
-  //Initialize Routes
-  route.appRoute(router);
-};
